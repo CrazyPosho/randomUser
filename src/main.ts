0ee0529475a9322ff8830e20e022f4554ece4e93
CrazyPosho <davidjorgeagramonte@gmail.com>
2024-11-14 00:35:21 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import { ApiResponse } from "./index";
-import "./styles.css";
 import { fetchData } from "./api";
-const url = "https://randomuser.me/api/";
+import "./styles.css";
 
-const nameValue = document.getElementById("name") as HTMLElement;
-const cellValue = document.getElementById("cell") as HTMLElement;
-const emailValue = document.getElementById("email") as HTMLElement;
-const genderValue = document.getElementById("genderValue") as HTMLElement;
-const locationValue = document.getElementById("location") as HTMLElement;
-const btn = document.getElementById("btn") as HTMLButtonElement;
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="container">
     <h1>API Fetcher - Random User</h1>
    
@@ -37,17 +30,26 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     </div>
   </div>
 `;
-const displayData = async () => {
-  const data = await fetchData<ApiResponse>(url);
-  const user = data?.results[0];
-  if (!data) return;
-  nameValue.textContent = ` ${user?.name.first} ${user?.name.last}`;
-  cellValue.textContent = user?.cell ?? null;
-  emailValue.textContent = user?.email ?? null;
-  genderValue.textContent = user?.gender ?? null;
-  locationValue.textContent = user?.location.country ?? null;
-};
+  const url = "https://randomuser.me/api/";
 
-btn.addEventListener("click", () => {
-  displayData();
+  const nameValue = document.getElementById("name") as HTMLElement;
+  const cellValue = document.getElementById("cell") as HTMLElement;
+  const emailValue = document.getElementById("email") as HTMLElement;
+  const genderValue = document.getElementById("genderValue") as HTMLElement;
+  const locationValue = document.getElementById("location") as HTMLElement;
+  const btn = document.getElementById("btn") as HTMLButtonElement;
+  const displayData = async () => {
+    const data = await fetchData<ApiResponse>(url);
+    const user = data?.results[0];
+    if (!data) return;
+    nameValue.textContent = ` ${user?.name.first} ${user?.name.last}`;
+    cellValue.textContent = user?.cell ?? "No disponible";
+    emailValue.textContent = user?.email ?? "No disponible";
+    genderValue.textContent = user?.gender ?? "No disponible";
+    locationValue.textContent = user?.location.country ?? "No disponible";
+  };
+
+  btn.addEventListener("click", () => {
+    displayData();
+  });
 });
